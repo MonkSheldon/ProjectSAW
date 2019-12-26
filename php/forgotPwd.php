dd1420481cff15e224280844c21446b60086742a
pagenotfound96 <57998689+pagenotfound96@users.noreply.github.com>
2019-12-26 12:29:44 +0100

    Add files via upload

--- php/forgotPwd.php
+++ php/forgotPwd.php
@@ -3,16 +3,12 @@
         header('Location: forgotPwdHTML.php?err=1');
         exit();
     }
-
     $email = trim($_POST['email']);
-
     if (empty($email)) {
         header('Location: forgotPwdHTML.php?err=1');
         exit();
     }
-
     require_once('db/mysql_credentials.php');
-
     function generateRandomString($length) {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $randomString = '';
@@ -21,7 +17,6 @@
         }
         return $randomString;
     }
-
     function update_pwd($email, $db_connection) {
         $pwdOriginal = generateRandomString(8);
         $pwd = sha1($pwdOriginal);
@@ -36,20 +31,36 @@
         }
         return null;
     }
-
     $success = update_pwd($email, $con);
-
     mysqli_close($con);
+    if ($success)
+    {  
+        require 'pear/PHPMailer.php';
+        $mail= new PHPMailer;
+        $mail->setFrom('veronica.salvi.majo@gmail.com', 'veronica');
+        $mail->addAddress('pagnonilorenzo@libero.it',' ');
+        $mail->Subject  = 'First PHPMailer Message';
+        $mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
+        if(!$mail->send()) {
+        echo 'Message was not sent.';
+        echo 'Mailer error: ' . $mail->ErrorInfo;
+        } else {
+        echo 'Message has been sent.';
+        }
+      /*
+        ini_set('SMTP','stmp.gmail.com');
+        ini_set('sendmail_from','pagnonilorenzo@llibero.it');
+        ini_set('smtp_port',465);
 
-    if ($success) {
-        // Success message
         $sub = "Site - Reimpostazione password";
         $msg = "Gentile utente,\n
                 la sua nuova password è la seguente: ". $success. "\n
                 Consigliamo di cambiarla al prossimo accesso per
                 aumentare la sicurezza del suo account\n\nStaff";
-        $header = "From: Site <lorenzo.pagnoni.majo@gmail.com>\r\n";
+        $header = "From: Site <pagnonilorenzo@libero.it>\r\n";
         $s = mail($email, $sub, $msg, $header);
+
+        ini_restore('sendmail_from');
         VAR_DUMP($s);
         /*    header("Location: forgotPwdHTML.php?msg=1");
         else
